62a8fa0a9a77bdaf852158112882b5c9593834e7
Cassandra Wilcox <cassie@codehangar.io>
2017-10-28 19:26:50 -0400

    feat: show success message after store saved

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
-  res.render('index');
+  res.render('index', { title: 'Home' });
 };
 
 exports.addStore = (req, res) => {
@@ -10,8 +10,7 @@ exports.addStore = (req, res) => {
 };
 
 exports.createStore = async (req, res) => {
-  const store = new Store(req.body);
-  await store.save();
-  console.log('It worked!')
-  res.redirect('/');
+  const store = await (new Store(req.body)).save();
+  req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
+  res.redirect(`/store/${store.slug}`);
 };

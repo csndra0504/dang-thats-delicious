a5e39905c04e7039625dfc5401fac2ec6420b2e7
Cassandra Wilcox <cassie@codehangar.io>
2017-10-28 20:23:28 -0400

    feat: display all stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -2,15 +2,20 @@ const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
-  res.render('index', { title: 'Home' });
+  res.render('index', {title: 'Home'});
 };
 
 exports.addStore = (req, res) => {
-  res.render('editStore', { title: 'Add Store' });
+  res.render('editStore', {title: 'Add Store'});
 };
 
 exports.createStore = async (req, res) => {
   const store = await (new Store(req.body)).save();
   req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
   res.redirect(`/store/${store.slug}`);
+};
+
+exports.getStores = async (req, res) => {
+  const stores = await Store.find();
+  res.render('stores', {title: 'Stores', stores })
 };
